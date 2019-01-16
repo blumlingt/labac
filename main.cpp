efbd1c5e3e9d911048ae9d038e5707f1f5520046
blumlingt <46723238+blumlingt@users.noreply.github.com>
2019-01-16 11:11:19 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "Utils.h"
+#include "lab1.h"
+#include "lab2.h"
+#include "lab3.h"
+#include "lab4.h"
+#include "lab5.h"
+#include "lab6.h"
+#include "lab7.h"
+
+int main() {
+    Lab1* lab1 = new Lab1();
+    Lab2* lab2 = new Lab2();
+    Lab3* lab3 = new Lab3();
+    Lab4* lab4 = new Lab4();
+    Lab5* lab5 = new Lab5();
+    Lab6* lab6 = new Lab6();
+    Lab7* lab7 = new Lab7();
+
+//    lab1->main();
+//    lab2->main();
+//    lab3->main();
+//    lab4->main();
+//    lab5->main();
+//    lab6->main();
+//    lab7->main();
+
+}
